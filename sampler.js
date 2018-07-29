0408445d90ee59e9bfa6a84170c1bacfc1d9d2ba
Tomasz Czerminski <czerminski.tomasz@gmail.com>
2018-07-29 09:01:12 +0200

    Basic HTTP support

--- sampler.js
+++ sampler.js
@@ -1,29 +1,47 @@
+const url = "http://localhost:8080";
+
 const extractCacheStatistics = cache =>
     cache ? {
         size: cache.size,
         liveSize: cache.liveSize
-    } : { 
-        size: undefined,
-        liveSize: undefined
-    };
+    } : {
+            size: undefined,
+            liveSize: undefined
+        };
 
-const statistics = chrome.processes.onUpdated.addListener((processes) => {
-    if(processes) {
-        return Object
-                .entries(processes)
-                .map(entry => entry[1])
-                .map(process => {
-                    return {
-                        type: process.type,
-                        cpu: process.cpu,
-                        network: process.network,
-                        privateMemory: process.privateMemory,
-                        jsMemoryAllocated: process.jsMemoryAllocated,
-                        jsMemoryUsed: process.jsMemoryUsed,
-                        sqliteMemory: process.sqliteMemory,
-                        imageCache: extractCacheStatistics(process.imageCache),
-                        cssCache: extractCacheStatistics(process.cssCache)
-                    }
-                }).forEach(it => console.log(it))
+const createDto = process => {
+    return {
+        type: process.type,
+        cpu: process.cpu,
+        network: process.network,
+        privateMemory: process.privateMemory,
+        jsMemoryAllocated: process.jsMemoryAllocated,
+        jsMemoryUsed: process.jsMemoryUsed,
+        sqliteMemory: process.sqliteMemory,
+        imageCache: extractCacheStatistics(process.imageCache),
+        cssCache: extractCacheStatistics(process.cssCache)
+    }
+};
+
+const httpRequestCallback = xhr => () => console.log(xhr.responseText);
+
+const send = payload => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = httpRequestCallback(xhr);
+    xhr.send(payload);
+}
+
+const getAsArray = processes => Object
+    .entries(processes)
+    .map(entry => entry[1]);
+
+const statistics = chrome.processes.onUpdated.addListener(processes => {
+    if (processes) {
+        return getAsArray(processes)
+            .map(process => createDto(process))
+            .map(dto => JSON.stringify(dto))
+            .forEach(payload => send(payload));
     }
 });
